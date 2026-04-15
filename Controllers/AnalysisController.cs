349d3d775d25086ef393bad87cda9fd25fe41c7a
Long <vuminhl@sheridancollege.ca>
2026-04-14 22:09:18 -0400

    add UI for AnalyzeData and Recommendations

--- Controllers/AnalysisController.cs
+++ Controllers/AnalysisController.cs
@@ -14,6 +14,11 @@ public class AnalysisController: Controller
     }
         public IActionResult AnalyzeData()
     {
+        var dietData = new List<double> { 10, 12, 11, 13, 12, 11, 25 }; 
+        var exerciseData = new List<double> { 5, 7, 6, 8, 5, 7, 6 };
+
+    ViewBag.DietData = dietData;
+    ViewBag.ExerciseData = exerciseData;
         return View();
     }
     public IActionResult Recommendations()
